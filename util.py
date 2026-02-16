06961011c40d9e4f19ba67da2702d313d990f580
toma09to <toma09to@mail.toma09to.com>
2026-02-16 06:50:00 +0000

    pandas has no attribute SettingWithCopyWarning???

--- util.py
+++ util.py
@@ -5,7 +5,7 @@ import pandas as pd
 import warnings
 
 # dfの値は正常に変更されているため、無視する
-warnings.simplefilter('ignore', pd.errors.SettingWithCopyWarning)
+# warnings.simplefilter('ignore', pd.errors.SettingWithCopyWarning)
 
 def analyze_stock(df):
 
